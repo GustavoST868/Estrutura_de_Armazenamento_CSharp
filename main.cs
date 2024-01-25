7e83ae91f2d0bffc6b76efea8592767352b19004
Gustavo Santos Teixeira <136706241+Gustavovo17102003@users.noreply.github.com>
2024-01-25 13:48:34 -0300

    Update main.cs

--- main.cs
+++ main.cs
@@ -1,11 +1,15 @@
+// Used libraries
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 // Store data in general
 class Store
 {
     // Attributes
-    public List<string> data = new List<string>();
+    public List<string> data_text = new List<string>();
+    public List<int> data_number = new List<int>();
+    public List<string> data_compound = new List<string>();
 
     // Constructor
     public Store() { }
@@ -13,20 +17,20 @@ class Store
     // Add value
     public void Add(string item)
     {
-        data.Add(item);
-        Sort(); 
+        data_text.Add(item);
+        Sort();
     }
 
     // Remove value
     public void Remove(string item)
     {
-        data.Remove(item);
+        data_text.Remove(item);
     }
-    
+
     // Search data
     public void Search(string item)
     {
-        if(data.Contains(item))
+        if (data_text.Contains(item))
         {
             Console.WriteLine("Dado encontrado!");
         }
@@ -35,31 +39,31 @@ class Store
             Console.WriteLine("Dado não encontrado!");
         }
     }
-    
+
     // Clear data
     public void Clear()
     {
-        data.Clear();
+        data_text.Clear();
     }
-    
+
     // Get size
     public int GetSize()
     {
-        return data.Count;
+        return data_text.Count;
     }
-    
+
     // Sort data
     public void Sort()
     {
-        data.Sort();
+        data_text.Sort();
     }
-    
+
     // Get item with id
     public string GetItemId(int index)
     {
-        if (index >= 0 && index < data.Count)
+        if (index >= 0 && index < data_text.Count)
         {
-            return data[index];
+            return data_text[index];
         }
         else
         {
@@ -70,13 +74,53 @@ class Store
     // Print list
     public void Display()
     {
-        Console.WriteLine("{" + string.Join(", ", data) + "}");
+        Console.WriteLine("data_text__{ " + string.Join(" , ", data_text) + " }");
     }
-    
+
     // See if it's empty
     public bool IsEmpty()
     {
-        return data.Count == 0;
+        return data_text.Count == 0;
+    }
+
+    // Function to check the data type
+    public int CheckType(string str)
+    {
+        bool isText = str.All(char.IsLetter);
+        bool isNumber = double.TryParse(str, out _);
+
+        if (isText)
+        {
+            // If it is text
+            Console.WriteLine("É texto");
+            return 0;
+        }
+        else if (isNumber)
+        {
+            // If it is number
+            Console.WriteLine("É número");
+            return 1;
+        }
+        else
+        {
+            // If it is neither text nor number
+            Console.WriteLine("Não é nem texto nem número");
+            return -1;
+        }
+    }
+
+    // Convert to int
+    public int ConvertToInt(string str)
+    {
+        if (int.TryParse(str, out int result))
+        {
+            return result;
+        }
+        else
+        {
+            Console.WriteLine("Erro ao converter para int. Retornando 0.");
+            return 0;
+        }
     }
 }
 
@@ -92,8 +136,11 @@ class Program
         store.Add("asdfa");
         store.Add("3");
         store.Add("yuituit");
-        store.Search("3");
+        store.CheckType("222");
+        //store.Search("3");
         //store.Remove("3"); 
-        //store.Display();
+        //store.ConvertToInt("1");
+
+        store.Display();
     }
 }
