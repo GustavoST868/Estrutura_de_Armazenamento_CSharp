1cc8e4385994ef12c1dfd2857d82f9d603ced107
Gustavo Santos Teixeira <136706241+Gustavovo17102003@users.noreply.github.com>
2024-01-31 19:59:41 -0300

    .

---
+++ application/application/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+class Store
+{
+    //atttributes
+    List<string> datatext = new List<string>();
+    List<double> datadouble = new List<double>();
+    List<string> datacompound = new List<string>();
+
+    public Store() { }
+
+    void insert_filter(string data)
+    {
+
+    }
+
+    void remove(string data)
+    {
+
+    }
+
+    void search(string data)
+    {
+
+    }
+
+    void get(string data)
+    {
+
+    }
+
+    void print(string data)
+    {
+
+    }
+}
+
+
+class CheckType
+{
+    public CheckType() { }
+
+    int checkType()
+    {
+        return 0;
+    }
+}
+
+
+class Analyze
+{
+    public Analyze() { }
+
+}
+
+class Program
+{
+    static void Main() 
+    {
+      
+    }
+}
