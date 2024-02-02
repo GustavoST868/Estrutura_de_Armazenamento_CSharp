325a83134cad501524e9406437edc4fbd7cdcdee
Gustavo Santos Teixeira <136706241+Gustavovo17102003@users.noreply.github.com>
2024-02-02 18:40:05 -0300

    .

--- Program.cs
+++ Program.cs
@@ -1,43 +1,77 @@
-﻿using System;
+﻿//used libraries
+using System;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 class Store
 {
     // Attributes
     List<string> datatext = new List<string>();
-    List<double> datanumber = new List<double>();
+    List<string> datanumber = new List<string>();
     List<string> datacompound = new List<string>();
 
     public Store() { }
 
-    void insert_filter(string data)
+    public void insert_filter(string data)
     {
+        CheckType ct = new CheckType();
+        int type = ct.CheckTypeFunction(data);
 
+        switch (type)
+        {
+            case 0:
+                datanumber.Add(data);
+                break;
+            
+            case 1:
+                datatext.Add(data);
+                break;
+                
+            case -1:
+                datacompound.Add(data);
+                break;
+        }
     }
 
     void remove(string data)
     {
-
+        
     }
 
     void search(string data)
     {
-
+       
     }
 
     void get(string data)
     {
-
+       
     }
 
-    void print(string data)
+    public void print()
     {
+        Console.WriteLine("\nDataNumber:");
+        foreach (var number in datanumber)
+        {
+            Console.Write("|"+number+"|");
+        }
 
+        Console.WriteLine("\n\nDataText:");
+        foreach (var text in datatext)
+        {
+            Console.WriteLine("|"+text+"|");
+        }
+
+        Console.WriteLine("\nDataCompound:");
+        foreach (var compound in datacompound)
+        {
+            Console.WriteLine("|"+compound+"|");
+        }
     }
 
     void save(string data)
     {
-
+        
     }
 }
 
@@ -45,9 +79,23 @@ class CheckType
 {
     public CheckType() { }
 
-    int checkTypeFunction()
-    {
-        return 0;
+    public int CheckTypeFunction(string data)
+    {   
+        // number
+        if (Regex.IsMatch(data, @"^\d+$"))
+        {
+            return 0;
+        }
+        // text
+        else if (Regex.IsMatch(data, @"^[a-zA-Z]+$"))
+        {
+            return 1;
+        }
+        // compound
+        else
+        {
+            return -1;
+        }
     }
 }
 
@@ -55,14 +103,14 @@ class Analyze
 {
     // Attributes
     List<string> datatextcopy = new List<string>();
-    List<double> datanumbercopy = new List<double>();
+    List<string> datanumbercopy = new List<string>();
     List<string> datacompoundcopy = new List<string>();
     
-    public Analyze(List<string> datatext, List<string> datacompound, List<double> datanumber)
+    public Analyze(List<string> datatext, List<string> datacompound, List<string> datanumber)
     {
         datatextcopy = datatext;
         datacompoundcopy = datacompound;
-        datanumbercopy = datanumbercopy;
+        datanumbercopy = datanumber;
     }
 
     void media()
@@ -72,12 +120,12 @@ class Analyze
 
     void standardDeviation()
     {
-
+        
     }
 
     void frequency()
     {
-
+        
     }
 }
 
@@ -85,6 +133,11 @@ class Program
 {
     static void Main()
     {
-        
+        //test
+        Store store = new Store();
+        store.insert_filter("1");
+        store.insert_filter("2a");
+        store.insert_filter("a");
+        store.print();
     }
 }
