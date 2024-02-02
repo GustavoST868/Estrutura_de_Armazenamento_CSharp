256a9d2f76ea531aafc4fa650be6d5676a9e9d3d
Gustavo Santos Teixeira <136706241+Gustavovo17102003@users.noreply.github.com>
2024-02-02 18:56:51 -0300

    ..

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿//used libraries
+﻿// Used libraries
 using System;
 using System.Collections.Generic;
 using System.Text.RegularExpressions;
@@ -22,30 +22,72 @@ class Store
             case 0:
                 datanumber.Add(data);
                 break;
-            
+
             case 1:
                 datatext.Add(data);
                 break;
-                
+
             case -1:
                 datacompound.Add(data);
                 break;
         }
     }
 
-    void remove(string data)
+    public void remove(string data)
     {
-        
+        CheckType ct = new CheckType();
+        int type = ct.CheckTypeFunction(data);
+
+        switch (type)
+        {
+            case 0:
+                datanumber.Remove(data);
+                break;
+
+            case 1:
+                datatext.Remove(data);
+                break;
+
+            case -1:
+                datacompound.Remove(data);
+                break;
+        }
     }
 
-    void search(string data)
+    public void search(string data)
+{
+    CheckType ct = new CheckType();
+    int type = ct.CheckTypeFunction(data);
+
+    switch (type)
     {
-       
+        case 0:
+            if (datanumber.Contains(data) == true)
+            {
+                Console.WriteLine("\nDado encontrado!\n");
+            }
+            break;
+
+        case 1:
+            if (datatext.Contains(data) == true)
+            {
+                Console.WriteLine("\nDado encontrado!\n");
+            }
+            break;
+
+        case -1:
+            if (datacompound.Contains(data) == true)
+            {
+                Console.WriteLine("\nDado encontrado!\n");
+            }
+            break;
     }
+}
+
 
     void get(string data)
     {
-       
+        
     }
 
     public void print()
@@ -53,19 +95,19 @@ class Store
         Console.WriteLine("\nDataNumber:");
         foreach (var number in datanumber)
         {
-            Console.Write("|"+number+"|");
+            Console.Write("|" + number + "|");
         }
 
         Console.WriteLine("\n\nDataText:");
         foreach (var text in datatext)
         {
-            Console.WriteLine("|"+text+"|");
+            Console.WriteLine("|" + text + "|");
         }
 
         Console.WriteLine("\nDataCompound:");
         foreach (var compound in datacompound)
         {
-            Console.WriteLine("|"+compound+"|");
+            Console.WriteLine("|" + compound + "|");
         }
     }
 
@@ -80,7 +122,7 @@ class CheckType
     public CheckType() { }
 
     public int CheckTypeFunction(string data)
-    {   
+    {
         // number
         if (Regex.IsMatch(data, @"^\d+$"))
         {
@@ -105,7 +147,7 @@ class Analyze
     List<string> datatextcopy = new List<string>();
     List<string> datanumbercopy = new List<string>();
     List<string> datacompoundcopy = new List<string>();
-    
+
     public Analyze(List<string> datatext, List<string> datacompound, List<string> datanumber)
     {
         datatextcopy = datatext;
@@ -115,7 +157,7 @@ class Analyze
 
     void media()
     {
-        
+       
     }
 
     void standardDeviation()
@@ -125,7 +167,7 @@ class Analyze
 
     void frequency()
     {
-        
+       
     }
 }
 
@@ -133,11 +175,12 @@ class Program
 {
     static void Main()
     {
-        //test
+        // test
         Store store = new Store();
         store.insert_filter("1");
         store.insert_filter("2a");
         store.insert_filter("a");
         store.print();
+        store.search("a");
     }
 }
